0c2e1e3ab4c18b593d053c50825cbb4bbdbe64f9
Raminta Ališauskaitė <raminta.alisauskaite@gmail.com>
2023-09-26 20:28:11 +0300

    second commit

--- src/components/WeaponsCont.js
+++ src/components/WeaponsCont.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import {useSelector} from "react-redux";
 
 const WeaponsCont = () => {
+
+    const loggedPlayerMoney = useSelector(state=>state.player.player.money);
+
+    async function generateItems(){
+        const res = await fetch('http://localhost:8000/generateItems');
+        const data = await res.json();
+        console.log(data);
+    }
+
     return (
         <div className="weapons">
-            weapons cont
+           <div className="d-flex justify-content-around  align-items-center w-100 h-75">
+                <div className="item">
+
+                        <img src="https://static.vecteezy.com/system/resources/previews/026/795/438/original/cartoon-game-sword-on-transparent-background-crossed-knight-sword-ancient-weapon-cartoon-design-free-png.png" alt=""/>
+
+                    <button className="takeBtn">TAKE</button>
+                </div>
+                <div className="item">
+                    <div>armour</div>
+                    <button className="takeBtn">TAKE</button>
+                </div>
+                <div className="item">
+                    <div>potion</div>
+                    <button className="takeBtn">TAKE</button>
+                </div>
+           </div>
+            <button className="generateBtn" onClick={generateItems}>GENERATE $100</button>
+            <div>Your money: <span className="moneySpan">${loggedPlayerMoney}</span></div>
         </div>
     );
 };
--- src/pages/LobbyPage.js
+++ src/pages/LobbyPage.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import NavBar from "../components/NavBar";
 import WeaponsCont from "../components/WeaponsCont";
 import InventoryCont from "../components/InventoryCont";
 import PlayersOnline from "../components/PlayersOnline";
+import {useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {updatePlayer} from "../features/player";
 
 const LobbyPage = () => {
+
+    const nav = useNavigate();
+    const dispatch = useDispatch();
+    const player = useSelector(state=>state.player.player);
+    console.log('player in redux',player);
+
+    useEffect(() => {
+        if(JSON.parse(localStorage.getItem('auto-save'))) {
+            const options = {
+                method: 'GET',
+                headers: {
+                    'content-type': 'application/json',
+                    authorization: localStorage.getItem('TOKEN')
+                }
+            }
+            fetch('http://localhost:8000/getPlayerInfo', options)
+                .then(res => res.json()).then(data => dispatch(updatePlayer(data.data)))
+        }
+
+    }, []);
+
     return (
-        <div>
-            <NavBar/>
-            <div className="lobby">
-                <div className="leftSide">
-                    <WeaponsCont/>
-                    <InventoryCont/>
+        <>
+            {player ?
+                <div>
+                    <NavBar/>
+                    <div className="lobby">
+                        <div className="leftSide">
+                            <WeaponsCont/>
+                            <InventoryCont/>
+                        </div>
+                        <PlayersOnline/>
+                    </div>
                 </div>
-                <PlayersOnline/>
-            </div>
-        </div>
+                :
+                <div className="page">
+                    <h1 className="p-2">YOU NEED TO LOGIN TO BE ABLE TO PLAY</h1>
+                    <button onClick={() => nav('/')}>GO TO LOGIN</button>
+                </div>
+            }
+
+        </>
+
     );
 };
 
